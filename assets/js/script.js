f66c83c3224556a1573418b9dbc652d708939faa
Daniel Shoup <79939129+danshoup@users.noreply.github.com>
2021-04-24 22:00:50 -0400

    most recent saves

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,29 +4,42 @@
 var questionsAll = [
   {
     question: "Commonly used data types DO NOT include:",
-    choices: ["strings", "booleans", "alerts", "numbers"],
+    choice: ["strings", "booleans", "alerts", "numbers"],
     answer: "alerts"
   },
   {
     question: "The condition in an 'if/else' statement is enclosed within ____________.",
-    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    choice: ["quotes", "curly brackets", "parentheses", "square brackets"],
     answer: "parentheses"
   },
   {
     question: "Arrays in JavaScript can be used to store ____________.",
-    choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+    choice: ["numbers and strings", "other arrays", "booleans", "all of the above"],
     answer: "all of the above"
   },
   {
     question: "String vavlues must be enclosed within _______________ when being assigned to variables.",
-    choices: ["commas", "curly braackets", "quotes", "parentheses"],
+    choice: ["commas", "curly braackets", "quotes", "parentheses"],
     answer: "quotes"
   },
   {
     question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-    choices: ["JavaScript", "terminal/bash", "for loops", "console log"],
+    choice: ["JavaScript", "terminal/bash", "for loops", "console log"],
     answer: "console log"
   },
 ];
 
 // Variables
+var score = 0;
+var questionIndex = 0;
+var timerElement = document.querySelector("#timer-count");
+var startButton = document.querySelector("#start-button");
+var quizQuestions = document.querySelector("#questions");
+var multipleChoice = document.querySelector(".choices");
+var timer; 
+var timerCount;
+
+// Adds event listener to start quiz and timer
+startButton.addEventListener("click", function() {
+
+})
